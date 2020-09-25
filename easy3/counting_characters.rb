2e00d90bebb8a45575b1e1b79593f5d9905a8308
Michael B Lawrence <007mike@gmail.com>
2020-09-24 23:25:22 -0400

    easy3 submissions

--- easy3/counting_characters.rb
+++ easy3/counting_characters.rb
@@ -1,6 +1,6 @@
 print "Please write word or multiple words: "
 
 words = gets.chomp
-characters = words.split(' ').join.length
+characters = words.split.join.length
 
 puts "There are #{characters} characters in \"#{words}\"."
