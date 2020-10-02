25a53a685c43db6380bb5c05f77ffe8cdad0aa88
Michael B Lawrence <007mike@gmail.com>
2020-10-01 22:49:49 -0400

    starting easy 6

---
+++ easy6/cute_angles.rb
@@ -0,0 +1,16 @@
+DEGREE = "\xC2\xB0"
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+
+
+def dms(flt)
+  total_seconds = (flt * MINUTES_PER_DEGREE * SECONDS_PER_MINUTE).round
+  degrees, remaining_seconds = total_seconds.divmod(MINUTES_PER_DEGREE * SECONDS_PER_MINUTE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  
+  format("#{degrees}#{DEGREE}%02d'%02d\"", minutes, seconds)
+
+end
+
+p dms(76.73) == %(76°43'48")
+p dms(360)
