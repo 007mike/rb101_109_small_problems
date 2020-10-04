d7647836827855562c528050f512ecefc0df7127
Michael Lawrence <007mike@gmail.com>
2020-10-04 15:01:54 -0400

    easy6 reverse array 2

---
+++ easy6/reverse2.rb
@@ -0,0 +1,18 @@
+def reverse(arr)
+  rev_arr = []
+  arr.length.times do |count|
+    rev_arr << arr[(arr.length - 1) - count]
+  end
+  rev_arr
+end
+
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []
+
+p list = [1, 3, 2]                      # => [1, 3, 2]
+p new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
