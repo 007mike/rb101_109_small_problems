ea01d81cee1578506b9f3c02be0b4d67253a62be
Michael Lawrence <007mike@gmail.com>
2020-10-05 19:25:13 -0400

    easy 7 combine lists

---
+++ easy7/interleaved.rb
@@ -0,0 +1,17 @@
+def interleave(arr1, arr2)
+  new_arr = []
+  arr1.length.times do |x|
+    new_arr << arr1[x] << arr2[x]
+  end
+  new_arr
+end
+
+def interleave_zip(arr1, arr2)
+  new_arr = arr1.zip(arr2).flatten
+end
+
+
+
+
+p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+p interleave_zip([1, 2, 3], ['a', 'b', 'c'])
