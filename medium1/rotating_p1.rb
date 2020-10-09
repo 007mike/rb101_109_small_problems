05b8bf7a3a73ea4fb2ac655a1be4360f5171e896
Michael B Lawrence <007mike@gmail.com>
2020-10-09 19:35:19 -0400

    starting medium 1

---
+++ medium1/rotating_p1.rb
@@ -0,0 +1,22 @@
+def rotate_array(arr)
+  new_arr = arr.dup
+  new_arr << new_arr.shift
+end
+
+def rotate_string(str)
+  rotate_array(str.split('')).join
+end
+
+def rotate_integer(num)
+  rotate_string(num.to_s).to_i
+end
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+p x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+p x  == [1, 2, 3, 4]                 # => true
+p rotate_string('Awesome Sauce')
+p rotate_integer(755999)
