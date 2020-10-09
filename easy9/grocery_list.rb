dd046bfbdf07fdde905c1395167f7402e744c62e
Michael B Lawrence <007mike@gmail.com>
2020-10-09 19:11:46 -0400

    finishing easy9

---
+++ easy9/grocery_list.rb
@@ -0,0 +1,8 @@
+def buy_fruit(arr)
+  fruits = []
+  arr.each { |subarr| subarr[1].times { fruits << subarr[0] } }
+  fruits
+end
+
+p buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) == 
+  ["apples", "apples", "apples", "orange", "bananas","bananas"]
