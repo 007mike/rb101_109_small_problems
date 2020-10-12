89d13a52f7e7cb2d300cab3d2a39fe0b96aaa309
Michael Lawrence <007mike@gmail.com>
2020-10-12 18:11:34 -0400

    1000 lights final touch ups

--- medium1/1000_lights2.rb
+++ medium1/1000_lights2.rb
@@ -1,10 +1,10 @@
 def lights_init(number)
-  lights_init_arr = Array.new(number, 0)
+  Array.new(number, 0)
 end
 
 def display_on_lights(arr)
-  arr.map.with_index do |val, idx|
-    (idx + 1) if val == 1
+  arr.map.with_index(1) do |val, idx|
+    idx if val == 1
   end.compact
 end
 
@@ -13,6 +13,7 @@ def lights_toggle(number, arr)
     1.upto(number) do |x|
       toggle = i * x
       break if toggle > number
+
       arr[toggle - 1] = arr[toggle - 1] == 1 ? 0 : 1
     end
   end
@@ -24,6 +25,5 @@ def lights_main(number)
   display_on_lights(lights)
 end
 
-
 p lights_main(5)
-p lights_main(10)
+p lights_main(100)
