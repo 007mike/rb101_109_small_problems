e55a04eab8fa5b7db49b276a544ae5dbe7a86968
Michael B Lawrence <007mike@gmail.com>
2020-10-12 19:30:50 -0400

    medium1 contd

--- medium1/diamonds.rb
+++ medium1/diamonds.rb
@@ -1,27 +1,19 @@
 def diamond(number)
-  spaces = number - 1
+  spaces = (number - 1) / 2
   stars = 1
   half = number / 2
   
   1.upto(half) do |row|
-    puts "#{' ' * (spaces / 2)}#{'*' * stars}#{' ' * (spaces / 2)}"
-    spaces -= 2
+    puts "#{' ' * spaces}#{'*' * stars}"
+    spaces -= 1
     stars += 2
   end
   
-  #spaces = 0
-  #stars = number
-
   (half + 1).downto(1) do |row|
-    puts "#{' ' * (spaces / 2)}#{'*' * stars}#{' ' * (spaces / 2)}"
-    spaces += 2
+    puts "#{' ' * spaces}#{'*' * stars}"
+    spaces += 1
     stars -= 2
   end
 end
-
-
-
-
-
 
 diamond(9)
