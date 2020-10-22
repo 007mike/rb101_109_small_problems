502f380f48e6b629396d8188500f0270234c234f
Michael B Lawrence <007mike@gmail.com>
2020-10-21 21:09:30 -0400

    transpose mxn

---
+++ advanced1/transposemxn.rb
@@ -0,0 +1,13 @@
+def transpose(array)
+  transposed = []
+  0.upto(array[0].size - 1) do |col|
+    transposed << 0.upto(array.size - 1).map {|row| array[row][col]}
+  end
+  transposed
+end
+
+p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) ==
+  [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+p transpose([[1]]) == [[1]]
